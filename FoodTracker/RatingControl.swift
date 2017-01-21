4da42a5643a1853023824f82c07875364539ba65
Vladimir Masko <soupdawgie@gmail.com>
2017-01-21 16:39:47 +0300

    Create image field

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Vladimir Masko on 21/01/17.
+//  Copyright © 2017 Vladimir Masko. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+  //MARK: Initialization
+}
